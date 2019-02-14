be7c7eff526f382c79b8225a2d9897e53bc63fce
Maurice Frank <maurice.frank@posteo.de>
2019-02-14 17:35:24 +0100

    Fixed solvre data structure

--- start.rs
+++ start.rs
@@ -8,18 +8,31 @@ pub struct Node {
     pub picked: bool, // Whether this node was picked, otherwise was chosen through simplification
 }
 
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct Solver {
-    pub clauses: Vec<Vec<i16>>,
+    pub clauses: Vec<Vec<(u16, bool)>>,
+    pub n_falses: Vec<usize>,
+    pub literals: Vec<Vec<u16>>,
     pub tree: Vec<Node>,
 }
 
 impl Solver {
     pub fn print(&self) {
-        for (i, clause) in self.clauses.iter().enumerate() {
-            print!("{}: [ ", i);
-            for lit in clause {
-                print!("{} ", lit);
+        println!("Clauses:");
+        for (i, (clause, n_false)) in self.clauses.iter().zip(self.n_falses.iter()).enumerate() {
+            print!("{}: [", i);
+            for (lit, sign) in clause {
+                let sign = if !sign { "¬" } else { " " };
+                print!("{}{} ", sign, lit);
+            }
+            println!("] #-{}",n_false);
+        }
+
+        println!("\n\nLiterals:");
+        for (lit, clvec) in self.literals.iter().enumerate() {
+            print!("{}: [", lit);
+            for clause in clvec {
+                print!("{} ", clause);
             }
             println!("]");
         }
@@ -29,22 +42,27 @@ impl Solver {
         let file_string = read_file(fname).unwrap();
         for line in file_string.lines() {
             if line.starts_with('p') || line.starts_with('c') { continue; }
-            let mut clause: Vec<i16> = Vec::new();
+            let mut clause: Vec<(u16, bool)> = Vec::new();
+            self.n_falses.push(0);
             for c in line.split_whitespace() {
-                let lit: i16 = c.parse().unwrap();
-                // Finished this lines clause
-                if lit == 0 {
+                let ilit: i16 = c.parse().unwrap();
+                let lit = ilit.abs() as usize;
+                if lit == 0 { // Finished this lines clause
                     self.clauses.push(clause);
                     break;
                 }
-                clause.push(lit);
+                if self.literals.len() < lit + 1 {
+                    self.literals.resize(lit + 1, vec![]);
+                }
+                self.literals[lit].push(self.clauses.len() as u16);
+                clause.push((lit as u16, ilit > 0));
             }
         }
     }
 }
 
-fn rec_test(mut solver: Solver, i: i16) -> bool {
-    solver.clauses.push(vec![i, i, i]);
+fn rec_test(mut solver: Solver, i: u16) -> bool {
+    solver.clauses.push(vec![(i, true), (i, true), (i, true)]);
     println!("{}", i);
     solver.print();
     if i == 4 {
@@ -85,11 +103,11 @@ fn main() {
     // let filename = String::from("damnhard.sdk.txt");
     // read_sudoku_file(filename);
 
-    let mut solver = Solver{ clauses:  vec![], tree: vec![]};
+    let mut solver: Solver = Default::default();
     let filename = String::from("test.sat");
     solver.add_sat_file(filename);
     solver.print();
-    rec_test(solver, 0);
-    println!("AYYYYYYY");
+    // rec_test(solver, 0);
+    // println!("AYYYYYYY");
     // solver.print();
 }
