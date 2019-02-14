10c305f28f9e8944900d9d80f102d6a8123022ba
Maurice Frank <maurice.frank@posteo.de>
2019-02-14 19:11:53 +0100

    beofre adding clause pointers

--- start.rs
+++ start.rs
@@ -3,7 +3,7 @@ use std::io::{Read, Result};
 
 #[derive(Clone)]
 pub struct Node {
-    pub literal: i16, // the id of the literal
+    pub literal: u16, // the id of the literal
     pub value: bool, // the chosen value for the literal
     pub picked: bool, // Whether this node was picked, otherwise was chosen through simplification
 }
@@ -11,27 +11,28 @@ pub struct Node {
 #[derive(Clone, Default)]
 pub struct Solver {
     pub clauses: Vec<Vec<(u16, bool)>>,
-    pub n_falses: Vec<usize>,
-    pub literals: Vec<Vec<u16>>,
+    pub n_not_falses: Vec<usize>,
+    pub literal_clauses: Vec<Vec<u16>>,
+    pub literal_values: Vec<Option<bool>>,
     pub tree: Vec<Node>,
 }
 
 impl Solver {
     pub fn print(&self) {
         println!("Clauses:");
-        for (i, (clause, n_false)) in self.clauses.iter().zip(self.n_falses.iter()).enumerate() {
+        for (i, (clause, n_not_false)) in self.clauses.iter().zip(self.n_not_falses.iter()).enumerate() {
             print!("{}: [", i);
             for (lit, sign) in clause {
                 let sign = if !sign { "¬" } else { " " };
                 print!("{}{} ", sign, lit);
             }
-            println!("] #-{}",n_false);
+            println!("] #-{}",n_not_false);
         }
 
         println!("\n\nLiterals:");
-        for (lit, clvec) in self.literals.iter().enumerate() {
+        for (lit, clauses) in self.literal_clauses.iter().enumerate() {
             print!("{}: [", lit);
-            for clause in clvec {
+            for clause in clauses {
                 print!("{} ", clause);
             }
             println!("]");
@@ -43,38 +44,43 @@ impl Solver {
         for line in file_string.lines() {
             if line.starts_with('p') || line.starts_with('c') { continue; }
             let mut clause: Vec<(u16, bool)> = Vec::new();
-            self.n_falses.push(0);
             for c in line.split_whitespace() {
                 let ilit: i16 = c.parse().unwrap();
                 let lit = ilit.abs() as usize;
                 if lit == 0 { // Finished this lines clause
+                    self.n_not_falses.push(clause.len());
                     self.clauses.push(clause);
                     break;
                 }
-                if self.literals.len() < lit + 1 {
-                    self.literals.resize(lit + 1, vec![]);
+                if self.literal_clauses.len() < lit + 1 {
+                    self.literal_clauses.resize(lit + 1, vec![]);
+                    self.literal_values.resize(lit + 1, None);
                 }
-                self.literals[lit].push(self.clauses.len() as u16);
+                self.literal_clauses[lit].push(self.clauses.len() as u16);
                 clause.push((lit as u16, ilit > 0));
             }
         }
     }
-}
 
-fn rec_test(mut solver: Solver, i: u16) -> bool {
-    solver.clauses.push(vec![(i, true), (i, true), (i, true)]);
-    println!("{}", i);
-    solver.print();
-    if i == 4 {
-        return true;
+    pub fn add_node(&mut self, literal: u16, value: bool, picked: bool) {
+        println!("⇒ {} ⇐", literal);
+        self.tree.push(Node{literal, value, picked});
+        self.literal_values[literal as usize] = Some(value);
     }
-    let retval = rec_test(solver.clone(), i + 1);
-    if i != 0 {
-        return retval;
-    } else {
-        println!("weeeeeee");
-        solver.print();
-        return true;
+
+    pub fn literal_value(&self, literal: u16) -> Option<bool> {
+        for node in &self.tree {
+            if node.literal == literal {
+                return Some(node.value.clone());
+            }
+        }
+        return None;
+    }
+
+    pub fn simplify(&mut self) {
+        for clauseid in self.n_not_falses.iter().position(|&x| x== 1) {
+            println!("Clause with id {} is uni", clauseid);
+        }
     }
 }
 
@@ -100,14 +106,9 @@ fn read_sudoku_file(fname: String) {
 }
 
 fn main() {
-    // let filename = String::from("damnhard.sdk.txt");
-    // read_sudoku_file(filename);
-
     let mut solver: Solver = Default::default();
     let filename = String::from("test.sat");
     solver.add_sat_file(filename);
     solver.print();
-    // rec_test(solver, 0);
-    // println!("AYYYYYYY");
-    // solver.print();
+    solver.simplify();
 }
