ffff5faf6b7bff67380f7525c2451a34682cd1d7
Maurice Frank <maurice.frank@posteo.de>
2019-02-25 15:59:16 +0100

    renamed log to stats

--- SAT.py
+++ SAT.py
@@ -6,7 +6,9 @@ from itertools import chain
 import json
 from time import time
 import os
+import logging
 
+log = logging.getLogger('SAT')
 
 # CONSTANTS and GLOBALS
 ERROR, DONE, NOT_DONE = 0, 1, 2
@@ -27,7 +29,7 @@ def value(literal: Literal):
     return not literal.startswith('-')
 
 
-class Log(object):
+class Stats(object):
     def __init__(self, solver: int, log_file='SAT.log'):
         self.solver = solver
         self.log_file = log_file
@@ -73,9 +75,10 @@ class Log(object):
                 ))
 
 
-def assign(clauses: List[Clause], solution: List[Literal], to_set_true: Literal, logger: Log)\
+def assign(clauses: List[Clause], solution: List[Literal], to_set_true: Literal, stats: Stats)\
         -> (List[Clause], List[Literal]):
-    logger.assign_calls += 1
+    log.debug('assign: {}'.format(to_set_true))
+    stats.assign_calls += 1
     new_solution = solution.copy()
     new_solution.append(to_set_true)
     new_clauses = [clause.copy() for clause in clauses]
@@ -84,14 +87,13 @@ def assign(clauses: List[Clause], solution: List[Literal], to_set_true: Literal,
         if to_set_true in clause:  # Clause is now true
             new_clauses.remove(clause)
         else:  # Literal not important anymore for clause
-            #clause.pop(to_set_true, None)
             clause.pop(neg_literal(to_set_true), None)
     return new_clauses, new_solution
 
 
-def split(clauses: List[Clause], solution: List[Literal], value: bool, logger: Log, solver: int = 1)\
+def split(clauses: List[Clause], solution: List[Literal], value: bool, stats: Stats, solver: int = 1)\
         -> (List[Clause], List[Literal]):
-    logger.split_calls += 1
+    stats.split_calls += 1
     next_literal = 0
     if solver == 1:
         next_literal = next(iter(clauses[0].keys()))
@@ -100,12 +102,12 @@ def split(clauses: List[Clause], solution: List[Literal], value: bool, logger: L
     elif solver == 3:
         pass
     to_set_true = next_literal if value else neg_literal(next_literal)
-    return assign(clauses, solution, to_set_true, logger=logger)
+    return assign(clauses, solution, to_set_true, stats=stats)
 
 
-def unit_propagation(clauses: List[Clause], solution: List[Literal], logger: Log)\
+def unit_propagation(clauses: List[Clause], solution: List[Literal], stats: Stats)\
         -> (int, List[Clause], List[Literal]):
-    logger.unit_calls += 1
+    stats.unit_calls += 1
     simplified = False
     status = DONE
     while not simplified:
@@ -114,12 +116,12 @@ def unit_propagation(clauses: List[Clause], solution: List[Literal], logger: Log
             if len(clause) == 1:
                 simplified = False
                 status = NOT_DONE
-                clauses, solution = assign(clauses, solution, next(iter(clause.keys())), logger=logger)
+                clauses, solution = assign(clauses, solution, next(iter(clause.keys())), stats=stats)
                 break
     return status, clauses, solution
 
 
-def remove_tautologies(clauses: List[Clause], logger: Log) -> List[Clause]:
+def remove_tautologies(clauses: List[Clause], stats: Stats) -> List[Clause]:
     n_tautologies = 0
     for clause in clauses:
         counts = {}
@@ -129,14 +131,14 @@ def remove_tautologies(clauses: List[Clause], logger: Log) -> List[Clause]:
                 clauses.remove(clause)
                 n_tautologies += 1
                 break
-    logger.number_tautologies += n_tautologies
+    stats.number_tautologies += n_tautologies
     return clauses
 
 
-def _solve(clauses: List[Clause], solution: List[Literal], logger: Log, solver: int = 1) -> List[Literal] or bool:
-    logger.solve_calls += 1
-    logger.size_solution = len(solution)
-    logger.update()
+def _solve(clauses: List[Clause], solution: List[Literal], stats: Stats, solver: int = 1) -> List[Literal] or bool:
+    stats.solve_calls += 1
+    stats.size_solution = len(solution)
+    stats.update()
 
     # If no clauses left to be satisfied, then we finished
     if len(clauses) == 0:
@@ -146,17 +148,17 @@ def _solve(clauses: List[Clause], solution: List[Literal], logger: Log, solver: 
     if any(len(clause) == 0 for clause in clauses):
         return False
 
-    s, clauses, solution = unit_propagation(clauses, solution, logger)
+    s, clauses, solution = unit_propagation(clauses, solution, stats)
     if s == NOT_DONE:
-        return _solve(clauses, solution, logger, solver)
+        return _solve(clauses, solution, stats, solver)
     else:
-        return _solve(*split(clauses, solution, True, logger), logger=logger, solver=solver) \
-               or _solve(*split(clauses, solution, False, logger), logger=logger, solver=solver)
+        return _solve(*split(clauses, solution, True, stats), stats=stats, solver=solver) \
+               or _solve(*split(clauses, solution, False, stats), stats=stats, solver=solver)
 
 
-def solve(clauses: List[Clause], logger: Log, solver: int = 1) -> List[Literal] or bool:
-    clauses = remove_tautologies(clauses, logger=logger)
-    return _solve(clauses, [], logger=logger, solver=solver)
+def solve(clauses: List[Clause], stats: Stats, solver: int = 1) -> List[Literal] or bool:
+    clauses = remove_tautologies(clauses, stats=stats)
+    return _solve(clauses, [], stats=stats, solver=solver)
 
 
 def parse_dimacs_line(line: str) -> Clause:
@@ -182,16 +184,16 @@ def count_literals(clauses: List[Clause]) -> int:
 
 def solve_files(fnames: List[str], solver: int = 1, verbose: bool = False, log_file: str = 'SAT.log')\
         -> List[Literal] or bool:
-    logger = Log(solver, log_file=log_file)
+    stats = Stats(solver, log_file=log_file)
     clauses = parse_dimacs_files(fnames)
-    logger.number_clauses = len(clauses)
-    logger.number_literals = count_literals(clauses)
-    solution = solve(clauses, solver=solver, logger=logger)
-    logger.solution = solution
+    stats.number_clauses = len(clauses)
+    stats.number_literals = count_literals(clauses)
+    solution = solve(clauses, solver=solver, stats=stats)
+    stats.solution = solution
     if verbose:
-        logger.report()
-    del logger.solution
-    logger.save()
+        stats.report()
+    del stats.solution
+    stats.save()
     return solution
 
 
@@ -202,6 +204,7 @@ def solve_file(fname: str, solver: int, verbose: bool, log_file: str) -> List[Li
 def parse_args():
     parser = ArgumentParser(description="General SAT solver (World record as of 2021)")
     parser.add_argument('-v', dest='verbose', action='store_true')
+    parser.add_argument('-vv', dest='debug', action='store_true')
     parser.add_argument('-S', dest='solver', help='The Solver to use.', type=int, choices=[1, 2, 3], default=1,
                         required=True)
     parser.add_argument('filenames', help='The text file to parse from.', type=str, nargs='+')
